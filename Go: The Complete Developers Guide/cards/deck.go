8e547cdbdc32925a828183ad392c6484ebb2f854
Bryan Smith <bryanesmith@gmail.com>
2025-12-14 17:49:39 -0500

    progress on decks

--- Go: The Complete Developers Guide/cards/deck.go
+++ Go: The Complete Developers Guide/cards/deck.go
@@ -1,16 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
-// Create a new type of 'deck'
-// which is a slice of strings
 type deck []string
+
+var Separator string = ","
 
 func newDeck() deck {
 	cards := deck{}
 
 	suits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	values := []string{"Ace", "Two", "Three", "Four"}
+	values := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	for _, suit := range suits {
 		for _, value := range values {
@@ -21,10 +27,17 @@ func newDeck() deck {
 	return cards
 }
 
-func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+func newDeckFromFile(filename string) (deck, error) {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
 	}
+	ss := string(bs)
+	return strings.Split(ss, Separator), nil
+}
+
+func (d deck) print() {
+	fmt.Println(d.toString())
 }
 
 func (d deck) deal(size int) (deck, deck) {
@@ -32,4 +45,22 @@ func (d deck) deal(size int) (deck, deck) {
 		return d, []string{}
 	}
 	return d[:size], d[size:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, Separator)
+}
+
+func (d deck) saveToFile(filename string) error {
+	b := []byte(d.toString())
+	return os.WriteFile(filename, b, 0666)
+}
+
+func (d deck) shuffle() {
+	src := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(src)
+	for i := range d {
+		j := r.Intn(len(d) - 1)
+		d[i], d[j] = d[j], d[i]
+	}
 }
--- Go: The Complete Developers Guide/cards/main.go
+++ Go: The Complete Developers Guide/cards/main.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	cards := newDeck()
+	cards.shuffle()
 	hand, cards := cards.deal(5)
-
 	hand.print()
-	cards.print()
+
+	filename := "my_cards.txt"
+	cards.saveToFile(filename)
+	newDeck, err := newDeckFromFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	newDeck.print()
 }
