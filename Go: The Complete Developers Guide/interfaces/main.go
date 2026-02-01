1810cca0408cbe9e065db9524f2bfebbeb9523de
Bryan Smith <bryanesmith@gmail.com>
2026-02-01 15:17:09 -0500

    sample app with bot interface

---
+++ Go: The Complete Developers Guide/interfaces/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func (englishBot) getGreeting() string {
+	// custom logic
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	// custom logic
+	return "Hola!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
