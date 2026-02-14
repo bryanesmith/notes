753eba748c02f1395a449e0d51b425f2fc30c6c3
Bryan Smith <bryanesmith@gmail.com>
2026-02-14 04:36:11 -0500

    exercise using interfaces

---
+++ Go: The Complete Developers Guide/http/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+type logWriter struct{}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
+}
+
+func main() {
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	lw := logWriter{}
+
+	len, _ := io.Copy(lw, resp.Body)
+	fmt.Println("Wrote", len, "bytes")
+}
